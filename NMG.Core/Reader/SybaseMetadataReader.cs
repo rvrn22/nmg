2d4d7234b62b0cc0a86ed813cb30d46ffa67721d
sca_tone <swiftanthony@hotmail.com>
2013-02-11 09:26:32 +0000

    Added foreign key support for sybase.

--- NMG.Core/Reader/SybaseMetadataReader.cs
+++ NMG.Core/Reader/SybaseMetadataReader.cs
@@ -28,22 +28,25 @@ namespace NMG.Core.Reader
                     using (var tableDetailsCommand = sqlCon.CreateCommand())
                     {
                         var sqlText = @"SELECT 
-    [default] default_value, c.column_name columnName, 
-    d.domain_name DataTypeName, width ColumnSize, scale columnPrecision,
-    convert(bit, CASE WHEN pkey = 'Y' THEN 1 ELSE 0 END) IsKey, 
-    convert(bit, CASE WHEN nulls = 'Y' THEN 1 ELSE 0 END) AllowDBNull,
-    convert(bit, CASE WHEN [default] = 'AUTOINCREMENT' THEN 1 ELSE 0 END) IsIdentity,
-    convert(bit, CASE WHEN [default] = 'NEWID()' THEN 1 ELSE 0 END) IsGenerated
-FROM sys.syscolumn c 
-    inner join sys.systable t on t.table_id = c.table_id
-    inner join sys.sysdomain d on c.domain_id = d.domain_id
-inner join sysobjects o on t.object_id = o.id
-inner join sys.sysuser u on u.user_id = o.uid
-WHERE t.table_name = '{0}' 
-and u.user_name = '{1}'";
+	                                    [default] default_value, c.column_name columnName, 
+	                                    d.domain_name DataTypeName, width ColumnSize, scale columnPrecision,
+	                                    convert(bit, CASE WHEN pkey = 'Y' THEN 1 ELSE 0 END) IsKey, 
+	                                    convert(bit, CASE WHEN c.[nulls] = 'Y' THEN 1 ELSE 0 END) AllowDBNull,
+	                                    convert(bit, CASE WHEN c.[default] = 'AUTOINCREMENT' THEN 1 ELSE 0 END) IsIdentity,
+	                                    convert(bit, CASE WHEN c.[default] = 'NEWID()' THEN 1 ELSE 0 END) IsGenerated,
+                                        convert(bit, CASE WHEN fcol.foreign_column_id is null THEN 0 ELSE 1 END) IsForeignKey,
+                                        ISNULL(fk.[role],'') ConstraintName
+                                    FROM sys.syscolumn c 
+	                                    inner join sys.systable t on t.table_id = c.table_id
+	                                    inner join sys.sysdomain d on c.domain_id = d.domain_id
+                                        inner join sysobjects o on t.object_id = o.id
+                                        inner join sys.sysuser u on u.user_id = o.uid
+                                        left join sysfkcol fcol ON c.table_id = fcol.foreign_table_id and c.column_id = fcol.foreign_column_id
+                                        left join sysforeignkey fk ON fcol.foreign_table_id = fk.foreign_table_id AND fcol.foreign_key_id = fk.foreign_key_id
+                                    WHERE t.table_name = '{0}' 
+                                    and u.user_name = '{1}'";
                         tableDetailsCommand.CommandText =
-                            string.Format(sqlText,
-                                table.Name.Replace("'", string.Empty), owner);
+                            string.Format(sqlText,table.Name, owner);
 
                         sqlCon.Open();
 
@@ -56,12 +59,13 @@ and u.user_name = '{1}'";
                             var name = dr["columnName"].ToString();
                             var isNullable = (bool) dr["AllowDBNull"];
                             var isPrimaryKey = dr["IsKey"] as bool?;
-
+                            var isForeignKey = (bool)dr["IsForeignKey"];
                             var dataType = dr["DataTypeName"].ToString();
                             var dataLength = Convert.ToInt32(dr["columnSize"]);
                             var dataPrecision = Convert.ToInt32(dr["columnSize"]);
                             var dataScale = Convert.ToInt32(dr["columnPrecision"]);
                             var isIdentity = dr["IsIdentity"] as bool?;
+                            var constraintName = dr["ConstraintName"].ToString();
                             var isUnique = false; //(bool) dr["IsKey"];
                             var mappedType = m.MapFromDBType(ServerType.Sybase, dataType, dataLength, dataPrecision,
                                                              dataScale);
@@ -82,13 +86,15 @@ and u.user_name = '{1}'";
                                         Name = name,
                                         IsNullable = isNullable,
                                         IsPrimaryKey = isPrimaryKey.GetValueOrDefault(),
+                                        IsForeignKey = isForeignKey,
                                         MappedDataType = mappedType.ToString(),
                                         DataLength = dataLength,
                                         DataScale = dataScale,
                                         DataPrecision = dataPrecision,
                                         DataType = dataType,
                                         IsUnique = isUnique,
-                                        IsIdentity = isIdentity.GetValueOrDefault()
+                                        IsIdentity = isIdentity.GetValueOrDefault(),
+                                        ConstraintName = constraintName
                                     });
 
                         }
@@ -99,8 +105,8 @@ and u.user_name = '{1}'";
                     table.Owner = owner;
                     table.Columns = columns;
                     table.PrimaryKey = DeterminePrimaryKeys(table);
-                    table.ForeignKeys = new List<ForeignKey>(); // DetermineForeignKeyReferences(table);
-                    table.HasManyRelationships = new List<HasMany>(); // DetermineHasManyRelationships(table);
+                    table.ForeignKeys = DetermineForeignKeyReferences(table);
+                    table.HasManyRelationships = DetermineHasManyRelationships(table);
                 }
                 catch (Exception ex)
                 {
@@ -132,7 +138,7 @@ and u.user_name = '{1}'";
                         {
                             while (sqlDataReader.Read())
                             {
-                                tables.Add(new Table {Name = sqlDataReader.GetString(0)});
+                                tables.Add(new Table {Name = sqlDataReader.GetString(0).Replace("'", string.Empty)});
                             }
                         }
                     }
@@ -224,6 +230,126 @@ and u.user_name = '{1}'";
 
 
             return null;
+        }
+
+        private IList<ForeignKey> DetermineForeignKeyReferences(Table table)
+        {
+            var constraints = table.Columns.Where(x => x.IsForeignKey.Equals(true)).Select(x => x.ConstraintName).Distinct().ToList();
+            var foreignKeys = new List<ForeignKey>();
+            constraints.ForEach(c =>
+            {
+                var fkColumns = table.Columns.Where(x => x.ConstraintName.Equals(c)).ToArray();
+                var fk = new ForeignKey
+                {
+                    Name = fkColumns[0].Name,
+                    References = GetForeignKeyReferenceTableName(table.Name, fkColumns[0].Name),
+                    AllColumnsNamesForTheSameConstraint = fkColumns.Select(f => f.Name).ToArray()
+                };
+                foreignKeys.Add(fk);
+            });
+
+            Table.SetUniqueNamesForForeignKeyProperties(foreignKeys);
+
+            return foreignKeys;
+        }
+
+        private string GetForeignKeyReferenceTableName(string selectedTableName, string columnName)
+        {
+            object referencedTableName;
+
+            var conn = new OleDbConnection(_connectionStr);
+            conn.Open();
+            try
+            {
+                using (conn)
+                {
+                    OleDbCommand tableCommand = conn.CreateCommand();
+                    tableCommand.CommandText = String.Format(
+                        @"
+						SELECT pt.table_name PK_TABLE
+	                         , t.Table_name FK_TABLE
+	                         , fc.column_name FK_COLUMN_NAME
+	                         , fk.role CONSTRAINT_NAME
+                             , pc.column_name PK_COLUMN_NAME
+                        FROM
+	                        SYSFOREIGNKEY fk
+	                        INNER JOIN systable t ON t.table_id = fk.foreign_table_id
+	                        INNER JOIN sysfkcol fcol ON fcol.foreign_table_id = fk.foreign_table_id AND fcol.foreign_key_id = fk.foreign_key_id
+	                        INNER JOIN syscolumn fc	ON fc.column_id = fcol.foreign_column_id AND fc.Table_id = fcol.foreign_table_id
+	                        INNER JOIN systable pt ON pt.table_id = fk.primary_table_id
+                            INNER JOIN syscolumn pc on pc.column_id = fcol.primary_column_id and pc.Table_id = fk.primary_table_id
+                        WHERE t.Table_name = '{0}' AND fc.column_name = '{1}'",
+                        selectedTableName, columnName);
+                    referencedTableName = tableCommand.ExecuteScalar();
+                }
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return (string)referencedTableName;
+        }
+
+
+        private IList<HasMany> DetermineHasManyRelationships(Table table)
+        {
+            var hasManyRelationships = new List<HasMany>();
+            var conn = new OleDbConnection(_connectionStr);
+            conn.Open();
+            try
+            {
+                using (conn)
+                {
+                    using (var command = new OleDbCommand())
+                    {
+                        command.Connection = conn;
+                        command.CommandText =
+                            String.Format(
+                                @"
+						SELECT pt.table_name PK_TABLE
+	                         , t.Table_name FK_TABLE
+	                         , fc.column_name FK_COLUMN_NAME
+	                         , fk.role CONSTRAINT_NAME
+                        FROM
+	                        SYSFOREIGNKEY fk
+	                        INNER JOIN systable t ON t.table_id = fk.foreign_table_id
+	                        INNER JOIN sysfkcol fcol ON fcol.foreign_table_id = fk.foreign_table_id AND fcol.foreign_key_id = fk.foreign_key_id
+	                        INNER JOIN syscolumn fc	ON fc.column_id = fcol.foreign_column_id AND fc.Table_id = fcol.foreign_table_id
+	                        INNER JOIN systable pt ON pt.table_id = fk.primary_table_id
+                        WHERE pt.table_name = '{0}'
+                        ORDER BY 1, 2",
+                                table.Name);
+                        var reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            var constraintName = reader["CONSTRAINT_NAME"].ToString();
+                            var fkColumnName = reader["FK_COLUMN_NAME"].ToString();
+                            var existing = hasManyRelationships.FirstOrDefault(hm => hm.ConstraintName == constraintName);
+                            if (existing == null)
+                            {
+                                var newHasManyItem = new HasMany
+                                {
+                                    ConstraintName = constraintName,
+                                    Reference = reader.GetString(1)
+                                };
+                                newHasManyItem.AllReferenceColumns.Add(fkColumnName);
+                                hasManyRelationships.Add(newHasManyItem);
+
+                            }
+                            else
+                            {
+                                existing.AllReferenceColumns.Add(fkColumnName);
+                            }
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return hasManyRelationships;
         }
     }
 
